8d26c55fc81413e5e0f1322f557547f9e8e313ec
Matthew R. Scott <gldnspud@gmail.com>
2009-06-25 17:14:18 -0700

    Start writing tests for the model.

--- turtlecanvas/tests.py
+++ turtlecanvas/tests.py
@@ -1,7 +1,20 @@
 from django.test import TestCase
 
+import turtlecanvas.models as m
+
 
 class TurtleCanvasTests(TestCase):
 
-    def test_environment(self):
-        self.assert_(True)
+    def test_starts_empty(self):
+        assert len(m.Canvas.objects.all()) == 0
+        assert len(m.CanvasState.objects.all()) == 0
+
+    def test_canvas_creates_initial_state(self):
+        canvas = m.Canvas(
+            title='My fancy art',
+            width=400,
+            height=300,
+        )
+        canvas.save()
+        assert len(m.Canvas.objects.all()) == 1
+        assert len(m.CanvasState.objects.filter(canvas=canvas)) == 1
